e9c016b744a004177711088f1ea0bdcd2d5a4c5b
Patricia Pampanelli <patricia.pampanelli@gmail.com>
2017-10-13 22:16:31 +0000

    bin_spatial

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -101,6 +101,14 @@ def convert_rgb_to_hsv(image):
 	
 	return H, S, V
 	
+def convert_rgb_to_luv(image):
+	
+	hsv = cv2.cvtColor(image, cv2.COLOR_RGB2Luv)
+	L = hsv[:,:,0]
+	U = hsv[:,:,1]
+	V = hsv[:,:,2]
+	
+	return L, U, V
 	
 def perspective_transform(image):
 	
@@ -177,4 +185,27 @@ def color_hist(img, nbins = 32, bins_range= (0, 256)):
 	
 	# Return the individual histograms, bin_centers and feature vector
 	return rhist, ghist, bhist, bin_centers, hist_features
+	
+	
+# Define a function to compute color histogram features  
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+	# Convert image to new color space (if specified)
+	if color_space != 'RGB':
+		if color_space == 'HSV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+		elif color_space == 'LUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+		elif color_space == 'HLS':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+		elif color_space == 'YUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+		elif color_space == 'YCrCb':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+	else: feature_image = np.copy(img)			 
+	# Use cv2.resize().ravel() to create the feature vector
+	features = cv2.resize(feature_image, size).ravel() 
+	# Return the feature vector
+	return features
 	
