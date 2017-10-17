1ccfaca91fd4a247b397be9bebf5d2726d3a881d
Patricia Pampanelli <patricia.pampanelli@gmail.com>
2017-10-17 23:21:40 +0000

    Image - Color Spaces

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -8,7 +8,6 @@ import numpy as np
 def convert_to_gray(images):
 	images_gray = []
 	for image in images:	
-		# Convert BGR to HSV
 		gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 		images_gray.append(gray)
 	
@@ -28,6 +27,7 @@ def sobel_operator(image, orient='x', thresh_min=0, thresh_max=255, sobel_kernel
 		
 	# Rescale back to 8 bit integer
 	scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+	#scaled_sobel = np.max(abs_sobel)/255 
 	# Create a copy and apply the threshold
 	binary_output = np.zeros_like(scaled_sobel)
 	# Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
@@ -100,15 +100,27 @@ def convert_rgb_to_hsv(image):
 	V = hsv[:,:,2]
 	
 	return H, S, V
-	
+
+
 def convert_rgb_to_luv(image):
 	
-	hsv = cv2.cvtColor(image, cv2.COLOR_RGB2Luv)
-	L = hsv[:,:,0]
-	U = hsv[:,:,1]
-	V = hsv[:,:,2]
+	luv = cv2.cvtColor(image, cv2.COLOR_RGB2Luv)
+	L = luv[:,:,0]
+	U = luv[:,:,1]
+	V = luv[:,:,2]
 	
-	return L, U, V
+	return L, U, V	
+	
+
+def convert_rgb_to_lab(image):
+	
+	lab = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
+	L = lab[:,:,0]
+	A = lab[:,:,1]
+	B = lab[:,:,2]
+	
+	return L, A, B		
+	
 	
 def perspective_transform(image):
 	
@@ -141,71 +153,6 @@ def perspective_transform(image):
 	return binary_warped, Minv
 	
 	
-# Template Matching
-def find_matches(img, template_list):
-	# Define an empty list to take bbox coords
-	bbox_list = []
-	# Define matching method
-	# Other options include: cv2.TM_CCORR_NORMED', 'cv2.TM_CCOEFF', 'cv2.TM_CCORR',
-	#		 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED'
-	method = cv2.TM_CCOEFF_NORMED
-	# Iterate through template list
-	for temp in template_list:
-		# Read in templates one by one
-		tmp = mpimg.imread(temp)
-		# Use cv2.matchTemplate() to search the image
-		result = cv2.matchTemplate(img, tmp, method)
-		# Use cv2.minMaxLoc() to extract the location of the best match
-		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-		# Determine a bounding box for the match
-		w, h = (tmp.shape[1], tmp.shape[0])
-		if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-			top_left = min_loc
-		else:
-			top_left = max_loc
-		bottom_right = (top_left[0] + w, top_left[1] + h)
-		# Append bbox position to list
-		bbox_list.append((top_left, bottom_right))
-		# Return the list of bounding boxes
-		
-	return bbox_list
 	
 	
-# Define a function to compute color histogram features  
-def color_hist(img, nbins = 32, bins_range= (0, 256)):
-	# Compute the histogram of the image channels separately
-	hist_0 = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
-	hist_1 = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
-	hist_2 = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
-	# Generating bin centers
-	bin_edges = rhist[1]
-	bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
-	# Concatenate the histograms into a single feature vector
-	hist_features = np.concatenate((hist_0[0], hist_1[0], hist_2[0]))
-	
-	# Return the individual histograms, bin_centers and feature vector
-	return rhist, ghist, bhist, bin_centers, hist_features
-	
-	
-# Define a function to compute color histogram features  
-# Pass the color_space flag as 3-letter all caps string
-# like 'HSV' or 'LUV' etc.
-def bin_spatial(img, color_space='RGB', size=(32, 32)):
-	# Convert image to new color space (if specified)
-	if color_space != 'RGB':
-		if color_space == 'HSV':
-			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-		elif color_space == 'LUV':
-			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-		elif color_space == 'HLS':
-			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-		elif color_space == 'YUV':
-			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-		elif color_space == 'YCrCb':
-			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-	else: feature_image = np.copy(img)			 
-	# Use cv2.resize().ravel() to create the feature vector
-	features = cv2.resize(feature_image, size).ravel() 
-	# Return the feature vector
-	return features
 	
