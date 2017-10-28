7f8805b2ff03fbb33757bdf52ecc7f8bd7292c22
Patricia Pampanelli <patricia.pampanelli@gmail.com>
2017-10-28 00:29:08 +0000

    Add HOG function and histogram of colors components

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -153,26 +153,44 @@ def bin_spatial(img, color_space='RGB', size=(32, 32)):
 	else: feature_image = np.copy(img)			 
 	# Use cv2.resize().ravel() to create the feature vector
 	# https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.ravel.html
-	features = cv2.resize(feature_image, size)
+	features = cv2.resize(feature_image, size).ravel()
 	# Return the feature vector
-	return features
+	return features, feature_image
 	
 	
-def get_hog_features(image, orient = 9, pix_per_cell = 8, cell_per_block = 2, visualise=False, 
-					 feature_vector=True):
-	if visualise == True:
-		features, hog_image = hog(image, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-								  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
-								  visualise=True, feature_vector=False, block_norm="L2-Hys")
-		return features, hog_image
-	else:	  
-		features = hog(image, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
-					   cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
-					   visualise=False, feature_vector=feature_vector, block_norm="L2-Hys")
-		return features
+def get_hog_features(img, color_space='YUV'):
+	#print(image.shape)
+	# Call with two outputs if vis==True
+	if color_space != 'RGB':
+		if color_space == 'HSV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+		elif color_space == 'LUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+		elif color_space == 'Lab':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
+		elif color_space == 'HLS':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+		elif color_space == 'YUV':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+		elif color_space == 'YCrCb':
+			feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+	hog = cv2.HOGDescriptor((64,64), (16,16), (8,8), (8,8), 9)
+
+	return np.ravel(hog.compute(feature_image))
 	
 	
-	
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+	# Compute the histogram of the RGB channels separately
+	rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+	ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+	bhist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+	# Generating bin centers
+	bin_edges = rhist[1]
+	bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+	# Concatenate the histograms into a single feature vector
+	hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+	# Return the individual histograms, bin_centers and feature vector
+	return rhist, ghist, bhist, bin_centers, hist_features
 	
 	
 	
